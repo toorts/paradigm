754418bb0fad0c0a258a51d9a31a0f7380352a86
Martin House <torchester@gmail.com>
2023-09-05 23:26:47 +0500

    upd task 1

--- hw_1/task_1.py
+++ hw_1/task_1.py
@@ -4,13 +4,15 @@ numbers = [random.randint(-10, 10) for _ in range(10)]
 print(numbers)
 
 
-def sort_list_imperative(numbers: list[int]) -> None:
+def sort_list_imperative(numbers: list[int]) -> list[int]:
     n = len(numbers)
+
     for i in range(n - 1):
         for j in range(0, n - i - 1):
             if numbers[j] < numbers[j + 1]:
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
+    return numbers
 
-sort_list_imperative(numbers)
-print(numbers)
+
+print(sort_list_imperative(numbers))
